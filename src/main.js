1cb526585affe24cda37c21843e5d502b83d6ea2
ITDevSolution <jronceros.byj@gmail.com>
2018-08-15 17:42:37 -0500

    Importamos Vue/Resource y Vue/Routes para crear las rutas de los componentes y agregamos en la instansiacion el router el component y template

--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import HelloWorld from './components/HelloWorld';
+import Calculator from './components/Calculator';
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {
+      path: '/HelloWorld',
+      component : HelloWorld
+    },
+    {
+      path: '/Calculator',
+      component: Calculator
+    }
+  ]
+})
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  components: { App },
+  template: '<App/>',
   render: h => h(App)
 }).$mount('#app')
