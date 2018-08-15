9f54ab69c61e550a8290314ad9b4fc5a5928754b
ITDevSolution <jronceros.byj@gmail.com>
2018-08-15 17:41:22 -0500

    creando el disenio de la calculadora con bootstrap

---
+++ src/components/Calculator.vue
@@ -0,0 +1,104 @@
+<template>
+     
+     <div class="Calculadora">
+          <div class="container">
+               <div class="row">
+                    <div class="col-md-12">
+                         <h1>Calculadora con VueJS</h1>
+                    </div>
+               </div>
+               <div class="row">
+                    <div class="col-md-4">
+                         
+                    </div>
+                    <div class="col-md-4 ">
+                         <button type="button" class="btn btn-primary btn-lg btn-block">0</button>
+                    </div>
+                    <div class="col-md-4">
+
+                    </div>
+                    
+               </div>
+               <div class="row">
+                    <div class="col-md-4">
+                         
+                    </div>
+                    <div class="col-md-4 btn-group" role="group">
+                         <button type="button" class="btn btn-outline-secondary btn-lg col-md-4">C</button>
+                         <button type="button" class="btn btn-outline-secondary btn-lg col-md-4">+/-</button>
+                         <button type="button" class="btn btn-outline-secondary btn-lg col-md-4">%</button>
+                         <button type="button" class="btn btn-warning btn-lg col-md-4">&#247;</button>
+                    </div>
+                    
+                    <div class="col-md-4">
+
+                    </div>
+               </div>
+               <div class="row">
+                    <div class="col-md-4">
+                         
+                    </div>
+                    <div class="col-md-4 btn-group" role="group">
+                         <button type="button" class="btn btn-outline-secondary btn-lg col-md-4">0</button>
+                         <button type="button" class="btn btn-outline-secondary btn-lg col-md-4">0</button>
+                         <button type="button" class="btn btn-outline-secondary btn-lg col-md-4">0</button>
+                         <button type="button" class="btn btn-warning btn-lg col-md-4">-</button>
+                    </div>
+                    
+                    <div class="col-md-4">
+
+                    </div>
+               </div>
+               <div class="row">
+                    <div class="col-md-4">
+                         
+                    </div>
+                    <div class="col-md-4 btn-group" role="group">
+                         <button type="button" class="btn btn-outline-secondary btn-lg col-md-4">0</button>
+                         <button type="button" class="btn btn-outline-secondary btn-lg col-md-4">0</button>
+                         <button type="button" class="btn btn-outline-secondary btn-lg col-md-4">0</button>
+                         <button type="button" class="btn btn-warning btn-lg col-md-4">+</button>
+                    </div>
+                    
+                    <div class="col-md-4">
+
+                    </div>
+               </div>
+               <div class="row">
+                    
+                    <div class="col-md-4 btn-group" role="group">
+                         
+                    </div>
+                    <div class="col-md-4 btn-group" role="group" >
+                         <button type="button" class="btn btn-outline-secondary btn-lg col-md-4">0</button>
+                         <button type="button" class="btn btn-outline-secondary btn-lg col-md-4">0</button>
+                         <button type="button" class="btn btn-outline-secondary btn-lg col-md-4">0</button>
+                         <button type="button" class="btn btn-warning btn-lg col-md-4">=</button>
+                    </div>
+                    
+                    <div class="col-md-4">
+
+                    </div>
+                    
+               </div>
+     </div>
+          
+     </div>
+</template>
+
+<script>
+export default {
+  name: "Calculadora",
+  data() {
+    return {};
+  },
+  //Metodos
+  methods: {}
+};
+</script>
+
+<style>
+.Calculadora {
+  /*Estidos del componente*/
+}
+</style>
